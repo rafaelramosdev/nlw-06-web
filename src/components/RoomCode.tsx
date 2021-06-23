4ddf14326e439c1f72802b4bd5120ceaa7253996
Rafael Ramos <faelrramos@gmail.com>
2021-06-22 22:54:33 -0300

    add: room code component

---
+++ src/components/RoomCode.tsx
@@ -0,0 +1,34 @@
+import toast, { Toaster } from 'react-hot-toast';
+
+import copyImg from '../assets/images/copy.svg';
+
+import '../styles/components/room-code.scss';
+
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+
+    toast.success('Successfully copied!');
+  }
+
+  return (
+    <>
+      <Toaster
+          position="top-center"
+          reverseOrder={false}
+      />
+
+      <button className="room-code" onClick={copyRoomCodeToClipboard}>
+        <div>
+          <img src={copyImg} alt="Copiar código da sala" />
+        </div>
+
+        <span>Sala #{props.code}</span>
+      </button>
+    </>
+  )
+}
