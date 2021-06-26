e7e9b0cd2e7549c6f34f8465c5a4b9db5df52447
Rafael Ramos <faelrramos@gmail.com>
2021-06-25 23:47:43 -0300

    modified: modal component

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,20 +2,24 @@ import { ReactNode } from 'react';
 
 import ModalComponent from 'react-modal';
 
+import { useTheme } from '../../hooks/useTheme';
+
 import './styles.scss';
 
 type ModalProps = {
   isOpened: boolean;
   closeModal: () => void;
   children: ReactNode;
-}
+};
 
 ModalComponent.setAppElement('#root');
 
 export function Modal({ isOpened, closeModal, children }: ModalProps) {
+  const { theme } = useTheme();
+
   return (
     <ModalComponent
-      className="modal"
+      className={`modal ${theme === 'dark' ? 'dark' : ''}`}
       overlayClassName="overlay-modal"
       isOpen={isOpened}
       onRequestClose={closeModal}
